9e5de3f293f5ec34036736cc7e61cb752eff5712
tinyhoot <78366332+tinyhoot@users.noreply.github.com>
2022-12-05 16:19:42 +0100

    Solve Day 5.

---
+++ 2022/05/solution.py
@@ -0,0 +1,64 @@
+import copy
+import re
+
+from utils.solution import Solution
+
+
+class Day05Solution(Solution):
+
+    def _get_result(self, crates: list[list]) -> str:
+        result = ""
+        for stack in crates:
+            if len(stack) > 0:
+                result += stack[-1]
+
+        return result
+
+    def _move_crate(self, crates: list[list], target: int, destination: int):
+        crate = crates[target].pop()
+        crates[destination].append(crate)
+
+    def parse(self, raw: str):
+        raw = raw.rstrip("\n")
+        # The last number in the line before instructions start must signify the number of crate stacks.
+        num_cols = int(re.search(r"\d+\s*\n\n", raw)[0])
+        crates = [[] for _ in range(num_cols)]
+        crates_raw, moves_raw = raw.split("\n\n")
+
+        for line in crates_raw.split("\n"):
+            # The crate names, if they exist, show up at set intervals.
+            for idx in range(1, len(line), 4):
+                if line[idx].isalpha():
+                    stack = idx // 4
+                    crates[stack].insert(0, line[idx])
+
+        moves = []
+        for line in moves_raw.split("\n"):
+            moves.append(tuple(map(int, re.findall(r"\d+", line))))
+
+        return crates, moves
+
+    def part1(self, data) -> str:
+        crates, moves = data
+        crates = copy.deepcopy(crates)
+        for move in moves:
+            # Move the crates one by one.
+            for _ in range(move[0]):
+                self._move_crate(crates, move[1] - 1, move[2] - 1)
+
+        return self._get_result(crates)
+
+    def part2(self, data):
+        crates, moves = data
+        for move in moves:
+            num, target, destination = move[0], move[1] - 1, move[2] - 1
+            payload = crates[target][-num:]
+            crates[target] = crates[target][:-num]
+            crates[destination] = crates[destination] + payload
+
+        return self._get_result(crates)
+
+
+if __name__ == "__main__":
+    solution = Day05Solution()
+    solution.solve(False)
