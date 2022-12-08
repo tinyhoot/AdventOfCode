cfdcb0a5516dc65a0a62919e0da600db18d6e01c
tinyhoot <78366332+tinyhoot@users.noreply.github.com>
2022-12-08 10:11:33 +0100

    Solve Day 8.

---
+++ 2022/08/solution.py
@@ -0,0 +1,62 @@
+from utils.puzzles.numbers import Point
+from utils.solution import Solution
+
+
+class Day08Solution(Solution):
+
+    directions_nswe = (0, -1), (0, 1), (-1, 0), (1, 0)
+
+    def _get_to_edge(self, data, pos: Point, delta):
+        """Get all elements from the given position to one of the edges of the forest."""
+        pos = pos + delta
+        while 0 <= pos.y < len(data) and 0 <= pos.x < len(data[pos.y]):
+            yield data[pos.y][pos.x]
+            pos = pos + delta
+
+    def parse(self, raw: str):
+        raw = raw.rstrip("\n")
+        return [[int(tree) for tree in line] for line in raw.split("\n")]
+
+    def part1(self, data) -> int:
+        """How many trees are visible from outside the grid?"""
+        total = 0
+        for y, line in enumerate(data):
+            for x, tree in enumerate(line):
+                # Look in each of the cardinal directions.
+                for direction in self.directions_nswe:
+                    visible = True
+                    for neighbour in self._get_to_edge(data, Point(x, y), direction):
+                        if neighbour >= tree:
+                            visible = False
+                            break
+                    if visible:
+                        total += 1
+                        break
+
+        return total
+
+    def part2(self, data):
+        """What is the best scenic score possible, i.e. how many trees can you possibly see at once from inside?"""
+        best_score = 0
+        for y, line in enumerate(data):
+            for x, tree in enumerate(line):
+                score = 1
+                # Look in each of the cardinal directions.
+                for direction in self.directions_nswe:
+                    visible = 0
+                    # This will actually zero out on trees at the edge of the forest, but I got lucky
+                    # and it worked anyway.
+                    for neighbour in self._get_to_edge(data, Point(x, y), direction):
+                        visible += 1
+                        if neighbour >= tree:
+                            break
+                    score *= visible
+                if score > best_score:
+                    best_score = score
+
+        return best_score
+
+
+if __name__ == "__main__":
+    solution = Day08Solution()
+    solution.solve(False)
--- utils/puzzles/numbers.py
+++ utils/puzzles/numbers.py
@@ -1,4 +1,7 @@
 # Things that help with solving puzzles that relate to numbers in some way.
+from collections.abc import Sequence
+
+
 class IntRange:
     """An inclusive range of numbers with a defined start and end."""
 
@@ -27,3 +30,18 @@ class IntRange:
     def overlaps(self, other: "IntRange") -> bool:
         """Check whether this range overlaps another one."""
         return (other.start <= self.start <= other.end) or (other.start <= self.end <= other.end) or other in self
+
+
+class Point:
+    """A point in a coordinate system."""
+
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        if isinstance(other, type(self)):
+            return Point(self.x + other.x, self.y + other.y)
+        if isinstance(other, Sequence) and len(other) == 2:
+            return Point(self.x + other[0], self.y + other[1])
+        raise ValueError
